7aa86e42e263b444834d8ce4a99e5536355f8a06
vali mohammad <65686387+vali91-ux@users.noreply.github.com>
2021-04-15 12:34:17 -0400

    Add files via upload

---
+++ assets/data/MyInfo.ts
@@ -0,0 +1,9 @@
+import {mohammav} from '../../app/mohammav';
+export const MYINFO:mohammav={
+
+    id:'9915897823',
+    name:'vali mohammad',
+    login:'mohammav',
+    campus:'Davis',
+    assignment:'Aissignment4'
+}
